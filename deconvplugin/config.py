4c4a1d1565ecc821b46a7f728d895ebde7bf0acb
Luca Gortana <luca.gortana@gmail.com>
2025-01-15 12:02:00 +0100

    logger config

--- deconvplugin/config.py
+++ deconvplugin/config.py
@@ -11,5 +11,6 @@ try:
 
     logger.remove(0)
     logger.add(lambda msg: tqdm.write(msg, end=""), colorize=is_terminal)
+    logger.add(sys.stdout, format="{time} - {level} - {message}", filter="sub.module")
 except ModuleNotFoundError:
     pass
