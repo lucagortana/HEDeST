149cef975094eb08d4a65d84e7fab81afaadf195
Luca GORTANA <lgortana@node121.cluster-fon.interne.mines-paristech.fr>
2025-02-05 18:25:29 +0100

    test

--- run_hovernet2.sh
+++ run_hovernet2.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=hovernet
 #SBATCH --output=/cluster/CBIO/home/lgortana/deconv-plugin/log/segment_%j.log
 #SBATCH --error=/cluster/CBIO/home/lgortana/deconv-plugin/log/segment_%j.err
-#SBATCH --gres=gpu:P100:1
+#SBATCH --gres=gpu:1
 #SBATCH -p cbio-gpu
 #SBATCH --cpus-per-task=4
 
@@ -14,11 +14,6 @@ conda activate hovernet
 
 export LD_LIBRARY_PATH=/cluster/CBIO/home/lgortana/anaconda3/envs/hovernet/lib:$LD_LIBRARY_PATH
 
-cache_dir="cache"
-if [ -f "$cache_dir" ]; then
-    rm "$cache_dir"
-fi
-
 python hovernet/run_infer.py \
     --gpu='0,1' \
     --nr_types=6 \
@@ -27,6 +22,7 @@ python hovernet/run_infer.py \
     --model_mode=fast \
     --model_path=/cluster/CBIO/data1/lgortana/pretrained/seg_classif/hovernet_fast_pannuke_type_tf2pytorch.tar \
     wsi \
-    --input_dir=/cluster/CBIO/data1/lgortana/CytAssist_FFPE_Sagittal_Mouse_Brain/tif/ \
-    --output_dir=/cluster/CBIO/data1/lgortana/CytAssist_FFPE_Sagittal_Mouse_Brain/seg_json/ \
-    --input_mask_dir=/cluster/CBIO/data1/lgortana/CytAssist_FFPE_Sagittal_Mouse_Brain/mask/lvl3/
+    --input_dir=/cluster/CBIO/data1/lgortana/Xenium_V1_humanLung_Cancer_FFPE/tif/ \
+    --output_dir=/cluster/CBIO/data1/lgortana/Xenium_V1_humanLung_Cancer_FFPE/seg_json/ \
+    --input_mask_dir=/cluster/CBIO/data1/lgortana/Xenium_V1_humanLung_Cancer_FFPE/mask/lvl3/ \
+    --cache_path=/cluster/CBIO/data1/lgortana/cache
