b8487255b34a9e95636cd4f82d545d654c579f82
Luca Gortana <luca.gortana@gmail.com>
2025-02-28 12:45:32 +0100

    reincorporate conv classic

--- deconvplugin/model/cell_classifier.py
+++ deconvplugin/model/cell_classifier.py
@@ -9,6 +9,49 @@ from torch import Tensor
 from deconvplugin.model.base_cell_classifier import BaseCellClassifier
 
 
+class ConvNet(nn.Module):
+    """
+    A simple Convolutional Neural Network.
+
+    Attributes:
+        layers: Sequential layer containing convolutional, batch normalization, ReLU, and pooling layers.
+    """
+
+    def __init__(self):
+        """
+        Initializes the ConvNet model.
+        """
+
+        super(ConvNet, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(3, 8, 3, padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(8, 16, 3, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(16, 32, 3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Defines the forward pass.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Output tensor after applying the ConvNet layers.
+        """
+
+        return torch.flatten(self.layers(x), 1)
+
+
 class CellClassifier(BaseCellClassifier):
     def __init__(
         self,
@@ -30,72 +73,41 @@ class CellClassifier(BaseCellClassifier):
         super().__init__(num_classes, device)
         self.model_name = model_name
         self.hidden_dims = hidden_dims
+        self.size_edge = 64
 
-        self.backbone = timm.create_model(model_name, pretrained=True, num_classes=0)
+        if self.model_name == "convnet":
+            # Use custom ConvNet
+            self.backbone = ConvNet()
 
-        for param in self.backbone.parameters():
-            param.requires_grad = False
+            self.fc_layers = nn.Sequential(
+                nn.Linear(
+                    int(32 * (self.size_edge / 8) ** 2), int(8 * (self.size_edge / 8) ** 2)
+                ),  # 32 * (a/8) * (a/8), 8 * (a/8) * (a/8)
+                nn.ReLU(),
+            )
 
-        self.fc_layers = nn.Sequential()
-        input_dim = self.backbone(torch.randn(1, 3, 64, 64)).shape[1]
-        for i, hidden_dim in enumerate(self.hidden_dims):
-            self.fc_layers.add_module(f"fc_{i}", nn.Linear(input_dim, hidden_dim))
-            self.fc_layers.add_module(f"relu_{i}", nn.ReLU())
-            input_dim = hidden_dim
+            self.classifier = nn.Linear(int(8 * (self.size_edge / 8) ** 2), self.num_classes)
 
-        # Final classification layer
-        self.classifier = nn.Linear(input_dim, num_classes)
+        else:
+            self.backbone = timm.create_model(model_name, pretrained=True, num_classes=0)
+
+            for param in self.backbone.parameters():
+                param.requires_grad = False
+
+            self.fc_layers = nn.Sequential()
+            input_dim = self.backbone(torch.randn(1, 3, 64, 64)).shape[1]
+            for i, hidden_dim in enumerate(self.hidden_dims):
+                self.fc_layers.add_module(f"fc_{i}", nn.Linear(input_dim, hidden_dim))
+                self.fc_layers.add_module(f"relu_{i}", nn.ReLU())
+                input_dim = hidden_dim
+
+            # Final classification layer
+            self.classifier = nn.Linear(input_dim, num_classes)
 
     def forward(self, x: Tensor) -> Tensor:
         features = self.backbone(x)
+        if self.model_name == "convnet":
+            x = torch.flatten(x, 1)
         features = self.fc_layers(features)
 
         return F.softmax(self.classifier(features), dim=1)
-
-
-# if model_name == "convnet":
-#     # Use custom ConvNet
-#     self.backbone = ConvNet()
-#     backbone_output_dim = int(32 * (edge_size / 8) ** 2)  # Output size of ConvNet
-
-# class ConvNet(nn.Module):
-#     """
-#     A simple Convolutional Neural Network.
-
-#     Attributes:
-#         layers: Sequential layer containing convolutional, batch normalization, ReLU, and pooling layers.
-#     """
-
-#     def __init__(self):
-#         """
-#         Initializes the ConvNet model.
-#         """
-
-#         super(ConvNet, self).__init__()
-#         self.layers = nn.Sequential(
-#             nn.Conv2d(3, 8, 3, padding=1),
-#             nn.BatchNorm2d(8),
-#             nn.ReLU(),
-#             nn.MaxPool2d(2, 2),
-#             nn.Conv2d(8, 16, 3, padding=1),
-#             nn.BatchNorm2d(16),
-#             nn.ReLU(),
-#             nn.MaxPool2d(2, 2),
-#             nn.Conv2d(16, 32, 3, padding=1),
-#             nn.BatchNorm2d(32),
-#             nn.ReLU(),
-#             nn.MaxPool2d(2, 2),
-#         )
-
-#     def forward(self, x: torch.Tensor) -> torch.Tensor:
-#         """
-#         Defines the forward pass.
-
-#         Args:
-#             x (torch.Tensor): Input tensor.
-
-#         Returns:
-#             torch.Tensor: Output tensor after applying the ConvNet layers.
-#         """
-
-#         return torch.flatten(self.layers(x), 1)
